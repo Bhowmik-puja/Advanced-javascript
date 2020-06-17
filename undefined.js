a86ce3fe69a672dde4bd90f7f32187fea4d33328
Bhowmik-puja <bhowmikpuja27@gmail.com>
2020-06-17 23:41:24 +0600

    null vs undefined concept cleared

---
+++ undefined.js
@@ -0,0 +1,19 @@
+//let pakhi;
+
+// function add(num1, num2){
+//    console.log(num1+num2);
+//    return
+// }
+
+// function add(num1,num2){
+//     console.log(num1+num2);
+
+// }
+// const result = add(12);
+
+
+// const obj = {name:"kalia", ages: 25};
+// console.log(obj.phone);
+
+let fun = undefined;
+console.log(fun);
