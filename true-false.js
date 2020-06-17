442773e88e171ab1a4eac8b30e87ae2446abe49a
Bhowmik-puja <bhowmikpuja27@gmail.com>
2020-06-17 23:23:25 +0600

    truthy & falsy value

---
+++ true-false.js
@@ -0,0 +1,13 @@
+const name = "sul";
+if(name){
+    console.log("condition is true");
+}
+else{
+    console.log("condition is false");
+}
+
+//falsy 
+//false , 0 , "empty string", null, NaN, undefined
+
+//truthy
+//any number , "string" , []
